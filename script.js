64ad1e3b7f3d0a2b814088687b46fd49e3a562b1
leticijazepa <zepaleticija@gmail.com>
2022-02-24 14:52:45 +0200

    24.02

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function nolasa (m1,m2,m3){
         return false;
     }
 }
+function perimetrs(m1,m2,m3){
+    const m = nolasa();
+    m1=m.m1;m2=m.m2;m3=m.m3;
+    console.log(m1,m2,m3);
+    const p=m1+m2+m3;
+    console.log(p);
+    return(p);
+}
+function pusper(m1,m2,m3){
+   const pusper = perimetrs(m1,m2,m3)/2;
+   console.log(pusper);
+   return(pusper);
+}
+function laukums(m1,m2,m3){
+    const pusper = perimetrs(m1,m2,m3)/2;
+    let zlaukums = Math.sqrt(pusper*((pusper-m1) * (pusper-m2)* (pusper-m3)));
+    console.log(zlaukums);
+    return zlaukums;
+}
+//Funkcija,kura parbauda vai eksiste trijsturis ar nolasitajam malam function...
+//parbauda vai izpildas trijstura nevienadiba
+//atgreiz true vai false
+function trijsturis(m1,m2,m3){
+    if(m1<m2+m3&& m2<m1+m3&& m3<m1+m2){
+        return true;
+    }else{
+        return false;
+    }
+}
