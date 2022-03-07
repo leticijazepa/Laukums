02f7c36fa76bd4a613d467365b2b3401b281dd88
leticijazepa <zepaleticija@gmail.com>
2022-03-07 15:08:51 +0200

    oooo

--- script.js
+++ script.js
@@ -34,9 +34,45 @@ function laukums(m1,m2,m3){
 //parbauda vai izpildas trijstura nevienadiba
 //atgreiz true vai false
 function trijsturis(m1,m2,m3){
-    if(m1<m2+m3&& m2<m1+m3&& m3<m1+m2){
+    if(m1<m2+m3 && m2<m1+m3 && m3<m1+m2){
         return true;
     }else{
         return false;
     }
+}
+function rezultats(){
+const m=nolasa();
+m1=m.m1; m2=m.m2; m3=m.m3;
+console.log(m1,m2,m3);
+if(!nolasa()){
+    t="Nekorekti ievaddati, trījstūra malu vērtībām ir jābūt lielākām par 0";
+}else{
+    if(trijsturis(m1,m2,m3)==false){
+        t="Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+    }else{
+        t="Trījstūris ar malu garumiem "+m1+", "+m2+" un "+m3+" eksistē. "
+        if(m1==m2 && m2==m3){
+            t+="Tas ir vienādmalu trijstūris. "
+        }else{
+            if(m1==m2 || m2==m3 || m1==m3){
+                t+="Tas ir vienādsānu trijstūris. "
+            }
+        }
+        const p=perimetrs(m1,m2,m3);
+        const s=Math.round(laukums(m1,m2,m3)*100)/100;
+        t+="Perimetrs ir "+p+" un laukums ir "+s+".";
+    }
+}
+console.log(t);
+return t;
+}
+function izvadaTekstu(){
+    //jāizsauc funkcija rezultats()
+    const teksts = rezultats();
+    //elementā ar id izvada izvada tekstu
+    console.log(teksts);
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    raksti.innerHTML = teksts;
+    sakne.appendChild(raksti);
 }
